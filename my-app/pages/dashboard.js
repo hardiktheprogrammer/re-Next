294da5c9965ac648787b9a19ad197a56df48a34e
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-12 19:39:08 +0530

    SSR

--- my-app/pages/dashboard.js
+++ my-app/pages/dashboard.js
@@ -1,5 +1,14 @@
 import { useState, useEffect } from 'react';
 function Dashboard() {
-
-    const [isLoading,set]
+  const [isLoading, setIsLoading] = useEffect(true);
+  const [dashboardData, setDashboardData] = useState(null);
+  useEffect(() => {
+    async function fetchDashboardData() {
+      const response = await fetch('http://localhost:4000/dashboard');
+      const data = await response.json();
+      setDashboardData(data);
+      setIsLoading(false);
+    }
+    fetchDashboardData();
+  });
 }
