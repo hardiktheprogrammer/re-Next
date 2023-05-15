e6394831e6363a0946bba587d85ac6cbd04ba3f2
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-16 00:34:05 +0530

    SWR

--- my-app/pages/event.js
+++ my-app/pages/event.js
@@ -1,7 +1,16 @@
+import { initialize } from 'next/dist/server/lib/render-server';
+import { useState } from 'react';
 function EventList({ eventList }) {
+  const [events, setEvents] = useState(EventList);
+
+  const fetchSportsEvents = async () => {
+    const response = await fetch('http://localhost:4000/events?category=sports');
+    const data = await response.json();
+  };
   return (
     <>
-      <h1>List of Event</h1>
+      <button onClick={fetchSportsEvents}> sports Events</button>
+      <h1>List of Events</h1>
       {eventList.map((event) => {
         return (
           <div Key={event.id}>
