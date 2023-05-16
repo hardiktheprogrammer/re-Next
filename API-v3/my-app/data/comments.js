8cc2aa51e0a17353894cc2279f4c912acb37c427
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-16 22:03:42 +0530

    API get request

---
+++ API-v3/my-app/data/comments.js
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    text: 'this ia a commit',
+  },
+  {
+    id: 1,
+    text: 'this ia a commit',
+  },
+  {
+    id: 1,
+    text: 'this ia a commit',
+  },
+];
