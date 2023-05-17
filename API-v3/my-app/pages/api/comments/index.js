c95972d5dd8aec5e0d125a3abbb8825f2d80459d
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-17 22:47:19 +0530

    Dynamic API router

--- API-v3/my-app/pages/api/comments/index.js
+++ API-v3/my-app/pages/api/comments/index.js
@@ -1,4 +1,15 @@
 import { comments } from '../../../data/comments';
 export default function handler(req, res) {
-  res.status(200).json(comments);
+  if (req.method === 'GET') {
+    res.status(200).json(comments);
+  } else if (req.method === 'POST') {
+    const comment = req.body.comment;
+    const newComment = {
+      id: data.now(),
+      text: comment,
+    };
+
+    comments.push(newComment);
+    res.status(203).json(newComment);
+  }
 }
