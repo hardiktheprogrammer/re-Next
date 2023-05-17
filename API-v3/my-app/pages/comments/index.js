8c29ffafa5697378a396eb75dda9fc11416052c4
Restyled.io <commits@restyled.io>
2023-05-17 17:17:44 +0000

    Restyled by prettier

--- API-v3/my-app/pages/comments/index.js
+++ API-v3/my-app/pages/comments/index.js
@@ -1,20 +1,20 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 function CommentsPages() {
   const [comments, setComments] = useState([]);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const fetchComments = async () => {
-    const response = await fetch('/api/comments');
+    const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
   // first argument
   const submitComment = async () => {
-    const response = await fetch('/api/comments', {
-      method : 'POST',
-      body : JSON.stringify({comment}),
-      headers : {
-        'Content-Type' : 'application/json',
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
       },
     });
     const data = await response.json();
@@ -23,15 +23,17 @@ function CommentsPages() {
 
   return (
     <>
-      <input type="text" value={comment} onChange={
-    (e) => setComment(e.target.value)} />
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
       <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load Comments</button>
       {comments.map((comment) => {
         return <div key={comment.id}> {comment.text} </div>;
-})
-}
-< />
+      })}
+    </>
   );
 }
 export default CommentsPages;
