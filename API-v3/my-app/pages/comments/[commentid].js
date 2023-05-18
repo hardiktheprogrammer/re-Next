8ecb27c9b35213ad69aa87ad0f0e923558709ab4
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-18 05:57:35 +0530

    API delete request

--- API-v3/my-app/pages/comments/[commentid].js
+++ API-v3/my-app/pages/comments/[commentid].js
@@ -1,8 +1,8 @@
-import { comments } from '../../../data/comments';
+import { comments } from '../../data/comments';
 
 export default function handler(req, res) {
   const { commentId } = req.query;
-  const comment = comments.find((commentId) => comment.id === parentInt(commentId));
+  const comment = comments.find((comment) => comment.id === parseInt(commentId));
 
   res.status(200).json(comment);
 }
