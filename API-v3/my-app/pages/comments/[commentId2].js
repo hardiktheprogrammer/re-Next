73da3bf8528f8ceaa0582896b2e41d21373927bf
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-19 09:37:28 +0530

    API cache

---
+++ API-v3/my-app/pages/comments/[commentId2].js
@@ -0,0 +1,16 @@
+function comment({ comment }) {
+  return (
+    <div>
+      {comment.id}, {comment.text}
+    </div>
+  );
+}
+export default comment;
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { commentId: '1' } }, { params: { commentId: '1' } }, { params: { commentId: '1' } }],
+
+    fallback: false,
+  };
+}
