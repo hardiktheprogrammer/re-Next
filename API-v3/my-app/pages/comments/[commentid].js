ea97471456c7ba11017563d8cbad5e1c62aca5be
Restyled.io <commits@restyled.io>
2023-05-19 01:51:52 +0000

    Restyled by prettier

--- API-v3/my-app/pages/comments/[commentid].js
+++ API-v3/my-app/pages/comments/[commentid].js
@@ -1,17 +1,20 @@
-import {comments} from '../../data/comments';
+import { comments } from "../../data/comments";
 
 export default function handler(req, res) {
-  const {commentId} = req.query;
-  if (req.method === 'GET') {
-    const comment =
-        comments.find((comment) => comment.id === parseInt(commentId));
+  const { commentId } = req.query;
+  if (req.method === "GET") {
+    const comment = comments.find(
+      (comment) => comment.id === parseInt(commentId)
+    );
 
     res.status(200).json(comment);
-  } else if (req.method === 'DELETE') {
-    const deleteComment =
-        comment.find((comment) => comment.id === parseInt(commentId));
-    const index =
-        comments.findIndex((comment) => comment.id === parseInt(commentId));
+  } else if (req.method === "DELETE") {
+    const deleteComment = comment.find(
+      (comment) => comment.id === parseInt(commentId)
+    );
+    const index = comments.findIndex(
+      (comment) => comment.id === parseInt(commentId)
+    );
     comments.splice(index, 1);
     res.status(200).json(deleteComment);
   }
