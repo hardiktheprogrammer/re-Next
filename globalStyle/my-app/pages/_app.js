d279ac89c2a42be236b8408b0e2f598184c6e778
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-20 13:52:06 +0530

    API fetching

---
+++ globalStyle/my-app/pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css';
+import { Inter } from 'next/font/google';
+
+function Myapp({ Component, pageProps }) {
+  return <component {...pageProps} />;
+}
+export default Myapp;
---
+++ globalStyle/my-app/pages/about.js
@@ -0,0 +1,5 @@
+function About() {
+  return <button className="btn btn-success">about Page</button>;
+}
+
+export default About;
