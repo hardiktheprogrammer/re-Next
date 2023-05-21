207d665fb43800da1e49a86fd4ec1f45e6402b0e
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-21 20:39:25 +0530

    CSS in JS

--- globalStyle/my-app/pages/_app.js
+++ globalStyle/my-app/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.main.css';
 
+const theme = {
+  color: {
+    primary: '#355C7D',
+  },
+};
 function Myapp({ Component, pageProps }) {
-  return <component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default Myapp
+export default Myapp;
