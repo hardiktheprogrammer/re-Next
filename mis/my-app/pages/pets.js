30b838b4dba9b78e10520a6816b41fedf85cc1f9
Restyled.io <commits@restyled.io>
2023-05-24 22:08:29 +0000

    Restyled by clang-format

--- mis/my-app/pages/pets.js
+++ mis/my-app/pages/pets.js
@@ -1,18 +1,18 @@
 import Image from 'next/image';
+
 import img from '../public/1.jpg';
+
 function PetsPage() {
   return (
     <div>
       <Image src={img} placeholder="blur" alt="pet" width="100" height="200" />;
       {['1', '2', '3', '4', '5'].map((path) => {
-        return (
-          <div Key={path}>
-            <Image src={`/${path}.jpg`} alt="pet" width="100" height="200" />
-          </div>
+      return (<div Key = {path}><Image src = {`/${path}.jpg`} alt =
+                                     "pet" width = "100" height = "200" />
+              </div>
         );
       })}
-    </div>
-  );
+    </div>);
 }
 
 export default PetsPage;
