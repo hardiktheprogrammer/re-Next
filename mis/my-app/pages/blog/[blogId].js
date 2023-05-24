437fa6b818aea28e57ee21dab0473e403f546211
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-24 10:54:59 +0530

    Image Component

---
+++ mis/my-app/pages/blog/[blogId].js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+function Blog({ title, description }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name=" description " content={description} />
+      </Head>
+      <h1 ClassName="content"> Article</h1>
+    </>
+  );
+}
+export default Blog;
+
+export async function getStaticSideProps() {
+  return {
+    props: {
+      title: 'Article Title',
+      description: 'Article Description',
+    },
+  };
+}
