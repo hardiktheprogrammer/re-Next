be1d5d352b62481ec3290699f2ceb6a81c1ade30
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-25 22:20:48 +0530

    Preview Mode

---
+++ mis/my-app/pages/news/index.js
@@ -0,0 +1,15 @@
+import { Console } from 'console';
+
+function News({ data }) {
+  return <h1 className="content"> {data}</h1>;
+}
+
+export default News;
+export async function getStaticProps(context) {
+  Console.log('Running getStaticProps');
+  return {
+    props: {
+      data: context.preview ? 'List of  draft articles' : 'list of published articles',
+    },
+  };
+}
