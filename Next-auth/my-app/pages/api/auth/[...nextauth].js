41ded0cc0e51e63ac5277e3a40d18ea7085d1ef6
Restyled.io <commits@restyled.io>
2023-05-28 05:55:18 +0000

    Restyled by clang-format

--- Next-auth/my-app/pages/api/auth/[...nextauth].js
+++ Next-auth/my-app/pages/api/auth/[...nextauth].js
@@ -2,10 +2,10 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
-  providers: [
+  providers : [
     Providers.Github({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId : process.env.GITHUB_ID,
+      clientSecret : process.env.GITHUB_SECRET,
     }),
   ],
 });
