d441ad5ee8f9001efa9c5c81950193f1eb457698
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-28 15:45:02 +0530

    Next-auth

--- Next-auth/my-app/pages/page.js
+++ Next-auth/my-app/pages/page.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/globals.css';
 import { useSession } from 'next-auth/client';
 
 export default function Home() {
