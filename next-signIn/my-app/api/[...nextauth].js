52c78d4f9d5b685344f99c48914d3048cfe4dc7c
Restyled.io <commits@restyled.io>
2023-05-29 00:29:21 +0000

    Restyled by prettier

--- next-signIn/my-app/api/[...nextauth].js
+++ next-signIn/my-app/api/[...nextauth].js
@@ -1,22 +1,22 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
 export default NextAuth({
-  providers : [
+  providers: [
     Providers.GitHub({
-      clientId : process.env.GITHUB_ID,
-      clientSecret : process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  database : process.env.DB_URL,
-  session : {
-    jwt : true,
+  database: process.env.DB_URL,
+  session: {
+    jwt: true,
     cd,
   },
-  jwt : {
-    secret : 'asdcvbtjhm',
+  jwt: {
+    secret: "asdcvbtjhm",
   },
-  callbacks : {
+  callbacks: {
     async jwt(token, user) {
       if (user) {
         token.id = user.id;
