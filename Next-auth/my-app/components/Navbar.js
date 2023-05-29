2bcc1b5e1e826f3f27c3b2d2988e1f478ccd9d3f
Restyled.io <commits@restyled.io>
2023-05-29 08:18:56 +0000

    Restyled by clang-format

--- Next-auth/my-app/components/Navbar.js
+++ Next-auth/my-app/components/Navbar.js
@@ -1,9 +1,9 @@
+import {signIn, signOut} from 'next-auth';
 import Link from 'next/link';
-import { signIn, signOut } from 'next-auth';
 
 function Navbar() {
   const [session, loading] = useSession();
-  console.log({ session, loading });
+  console.log({session, loading});
   return (
     <nav className="header">
       <h1 className="logo">
@@ -30,32 +30,31 @@ function Navbar() {
           <li>
             <Link href="/api/auth/signin">
               <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signIn('github');
-                }}
-              >
-                Sign In
-              </a>
-            </Link>
-          </li>
+  onClick =
+      {
+        (e) => {
+          e.preventDefault();
+          signIn('github');
+        }
+      } > Sign In</a>
+            </Link><
+      /li>
         )}
         {session && (
           <li>
-            <Link href="/api/auth/signout">
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signOut();
-                }}
-              >
-                Sign Out
-              </a>
-            </Link>
-          </li>
+            <Link href="/api /
+          auth / signout ">
+      < a
+  onClick = {
+    (e) => {
+      e.preventDefault();
+      signOut();
+    }
+  } > Sign Out</a>
+            </Link></li>
         )}
-      </ul>
-    </nav>
+      </ul><
+            /nav>
   );
 }
 
--- Next-auth/my-app/pages/page.js
+++ Next-auth/my-app/pages/page.js
@@ -1,11 +1,12 @@
+import {useSession} from 'next-auth/client ';
 import Head from 'next/head';
 import Image from 'next/image';
+
 import styles from '../styles/globals.css';
-import { useSession } from 'next-auth/client ';
 
 export default function Home() {
   const [session, loading] = useSession();
-  console.log({ session, loading });
+  console.log({session, loading});
   return (
     <div className={styles.container}>
       <Head>
@@ -41,26 +42,26 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+  className = {styles.card} > <h2>Deploy & rarr;
+      </h2>
             <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
+      </a>
+        </div><
+      /main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          href="https:/ /
+          vercel.com
+      ? utm_source = create - next - app &utm_medium =
+                         default - template &utm_campaign =
+                             create - next - app "
+  target = "_blank"
+  rel = "noopener noreferrer" > Powered by{' '}<span className = {styles.logo}>
+        <Image src = "/vercel.svg" alt =
+             "Vercel Logo" width = {72} height = { 16 } />
           </span>
         </a>
-      </footer>
-    </div>
+      </footer>< /div>
   );
 }
