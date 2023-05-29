7a495f9e041d9b3e60f1aafa986e0520e0472b03
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-29 13:48:04 +0530

    sign In

--- Next-auth/my-app/pages/api/dashboard.js
+++
@@ -1,4 +0,0 @@
-function Dashboard() {
-  return <h1>Dahboard page</h1>;
-}
-export default Dashboard;
---
+++ Next-auth/my-app/pages/dashboard.js
@@ -0,0 +1,8 @@
+import { useSession } from 'next-auth/client';
+function Dashboard() {
+  const [session, loading] = useSession();
+  console.log({ session, loading });
+  return <h1>Dahboard page</h1>;
+}
+
+export default Dashboard;
