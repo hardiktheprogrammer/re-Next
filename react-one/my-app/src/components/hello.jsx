e7ed08eed7ad17ab8067988ce6651e65d154b59d
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-05 14:30:26 +0530

    function components

--- react-one/my-app/src/components/hello.jsx
+++ react-one/my-app/src/components/hello.jsx
@@ -1,6 +1,7 @@
-// function hello() {
-//   return <h1> Hardik sharma </h1>;
-// }
-const name = 'ashutosh';
-const hello = (props) => <h1>hardik my name is mai ek chutiyaa hun</h1>;
+const name = 'Hardik';
+
+function hello() {
+  return <h1> Hardik sharma {name}</h1>;
+}
+// const hello = (props) => <h1>hardik my name is mai ek chutiyaa hun</h1>;
 export default hello;
