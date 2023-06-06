3669f1d638e018fd815bbd06a70059e8f09bc5e0
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-06 14:56:51 +0530

    counter

--- react-one/my-app/src/App.jsx
+++ react-one/my-app/src/App.jsx
@@ -4,14 +4,16 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import Hello from './components/hello';
 import Message from './components/Message';
-import Profile from './components/profile';
+// import Profile from './components/profile';
+import Counter from './components/counter';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Profile name="sharma" lastname="ji" />
+      {/* <Profile name="sharma" lastname="ji" />    */}
       <Message />
+      <Counter />
     </div>
   );
 }
--- react-one/my-app/src/components/counter.jsx
+++ react-one/my-app/src/components/counter.jsx
@@ -1,21 +1,21 @@
-import {Component} from react 
+import react, { Component } from 'react';
 
 class Counter extends Component {
-    
-    constructor(x) {
+  constructor() {
+    super(); // super method
+    this.state = {
+      counter: 0,
+    };
+  }
 
-        super(); // super method 
-        this.state = {
-            counter: 0
-        }
-    }
-
-    render() {
-    return 
-        <h3> Counting 1 {this.state }</h3>
-        }
-    
-    
+  render() {
+    return (
+      <div>
+        <h3> Counting 1 {this.state.counter}</h3>
+        <button> Click</button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
