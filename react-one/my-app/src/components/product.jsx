8a9d11025e48657814ba11af28b21472bf93467c
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-09 23:09:16 +0530

    List rendering

--- react-one/my-app/src/components/product.jsx
+++ react-one/my-app/src/components/product.jsx
@@ -1,10 +1,8 @@
 export default function Product() {
   const products = ['macbook', 'Monitor ', 'Rassberry Pi ', '32 GB RAM'];
-  return (
-    <div>
-      {products.map((product) => (
-        <h3>{product}</h3>
-      ))}
-    </div>
-  );
+
+  const productlist = products.map((product) => <h3>{product}</h3>);
+
+  return;
+  <div>{productlist}</div>;
 }
