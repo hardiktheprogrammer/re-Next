4e92f17738e3ab1fb5830fa9296fee1f79673935
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-10 11:09:30 +0530

    Rendering list Object

--- react-one/my-app/src/components/product.jsx
+++ react-one/my-app/src/components/product.jsx
@@ -1,8 +1,7 @@
 export default function Product() {
   const products = ['macbook', 'Monitor ', 'Rassberry Pi ', '32 GB RAM'];
 
-  const productlist = products.map((product) => <h3>{product}</h3>);
+  const productList = products.map((product) => <h3 key={product}>{product}</h3>);
 
-  return;
-  <div>{productlist}</div>;
+  return <div>{productList}</div>;
 }
