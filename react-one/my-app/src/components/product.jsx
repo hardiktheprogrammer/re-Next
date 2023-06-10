59bb9789e9c0d027cc0d2e03a3ec0be163880a7f
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-10 15:29:45 +0530

    Rendering

--- react-one/my-app/src/components/product.jsx
+++ react-one/my-app/src/components/product.jsx
@@ -1,7 +1,19 @@
 export default function Product() {
-  const products = ['macbook', 'Monitor ', 'Rassberry Pi ', '32 GB RAM'];
+  //   const products = ['macbook', 'Monitor ', 'Rassberry Pi ', '32 GB RAM'];
 
-  const productList = products.map((product) => <h3 key={product}>{product}</h3>);
+  //   const productList = products.map((product) => <h3 key={product}>{product}</h3>);
 
-  return <div>{productList}</div>;
+  //   return <div>{productList}</div>;
+  // }
+
+  const products = [
+    { id: 1, name: '1- phone', price: 300 },
+
+    { id: 2, name: 'Samshung phone', price: 600 },
+    { id: 3, name: 'Ione Phone', price: 500 },
+  ];
+  const fruits = ['apple', 'android', 'Linux'];
+  const fruitList = fruits.map((fruits, index) => <h3 key={index}>{fruits}</h3>);
+
+  return <div>{fruitList}</div>;
 }
