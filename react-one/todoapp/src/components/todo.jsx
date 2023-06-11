309a5070479f4b9507a2e0a44242ae07b7a76cb8
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-12 00:20:11 +0530

    Listitem

--- react-one/todoapp/src/components/todo.jsx
+++ react-one/todoapp/src/components/todo.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react';
-
+import ListItem from '../components/ListItem';
 export default function Todo() {
+  const [todoList, setTodoList] = useState([]);
   const [todo, setTodo] = useState();
   const handleChange = (event) => {
     setTodo(event.target.value);
     console.log(todo);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let tempList = todoList;
+    tempList.push(todo);
+    setTodoList(tempList);
+    console.log(todoList);
+    setTodo('');
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input value={todo} onChange={handleChange} type="text"></input>
         <button type="submit"> Add </button>
       </form>
+      {todoList.map((item) => (
+        <ListItem></ListItem>
+      ))}
     </div>
   );
 }
