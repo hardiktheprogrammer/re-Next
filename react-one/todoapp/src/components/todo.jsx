0ded9cffe1da31057c15c1f1033b173ca3294852
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-11 19:44:29 +0530

    todo

---
+++ react-one/todoapp/src/components/todo.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function Todo() {
+  const [todo, setTodo] = useState();
+
+  return (
+    <div>
+      <form></form>
+    </div>
+  );
+}
+// export default Todo;
