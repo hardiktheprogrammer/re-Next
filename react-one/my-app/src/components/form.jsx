7059330edd428e576c975ddca7ea2f15f9591b64
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-11 18:00:08 +0530

    handling user input

--- react-one/my-app/src/components/form.jsx
+++ react-one/my-app/src/components/form.jsx
@@ -9,16 +9,20 @@ class Form extends Component {
     this.setState({
       fistname: event.target.value,
     });
-
-    console.log(this.state.fistname);
   };
+  handleLastNameChange = (event) => {
+    this.setState({
+      lastname: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
         form
         <form>
-          <input onChange={this.handleChange} type="text" value="Fistlie"></input>
-          <input value={this.state.lastname} type="text" onChange={this.handleLastNameChange}></input>
+          <input onChange={this.handleChange} type="text" value="Fistlie" />
+          <input value={this.state.lastname} type="text" onChange={this.handleLastNameChange} />
         </form>
       </div>
     );
