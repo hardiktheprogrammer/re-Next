99ee84680bbaf6174ded2880481ce397a3499018
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-12 16:04:34 +0530

    adding Header

--- Todoone/todoapp/src/App.jsx
+++ Todoone/todoapp/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
-import Header from './components/Header';
-function App() {}
+import Header from './src/components/Header';
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
 
 export default App;
---
+++ Todoone/todoapp/src/components/header.jsx
@@ -0,0 +1,6 @@
+function app() {
+
+    return 
+    <div></div>
+}
+export default app;
