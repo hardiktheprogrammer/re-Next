9c6a7daa95542d627a543c185548a8a9369f053f
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-12 13:24:55 +0530

    stylesheet

--- react-one/todoapp/src/App.jsx
+++ react-one/todoapp/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Todo from './components/todo';
 import './App.css';
+import InlineComponent from './components/inlineComponent';
 
 function App() {
   return (
     <div className="App">
       <Todo />
+      <InlineComponent />
     </div>
   );
 }
---
+++ react-one/todoapp/src/components/inlineComponent.jsx
@@ -0,0 +1,13 @@
+const header = {
+  color: 'blue',
+  fontSize: '50px',
+  textAlign: 'right',
+};
+
+export default function InlineComponent() {
+  return (
+    <div>
+      <h1 style={header}>this is the in line components</h1>
+    </div>
+  );
+}
