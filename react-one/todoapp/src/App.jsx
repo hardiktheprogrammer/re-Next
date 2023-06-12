3c6e826647678cbeef6daad942e483ca44516289
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-12 13:38:54 +0530

    stylesheetcomponents

--- react-one/todoapp/src/App.jsx
+++ react-one/todoapp/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import Todo from './components/todo';
 import './App.css';
 import InlineComponent from './components/inlineComponent';
-
+import StyleSheetComponent from './components/styelsheetcomponent';
 function App() {
   return (
     <div className="App">
       <Todo />
       <InlineComponent />
+      <StyleSheetComponent />
     </div>
   );
 }
